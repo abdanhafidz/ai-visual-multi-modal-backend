cd5f8891ae030f79db36b9783534f3aa48e1f932
abdanhafidz <abdan.hafidz@gmail.com>
2025-06-24 02:00:25 +0700

    remove: audio

--- factory/prediction_factory.go
+++ factory/prediction_factory.go
@@ -9,10 +9,20 @@ import (
 
 func NewPredictionModule() controller.PredictionController {
 	chatHistoryRepository := repositories.NewChatHistoryRepository(config.DB)
-	openAIService := services.NewOpenAIService(chatHistoryRepository, config.OpenAIClient)
-	replicateService := services.NewReplicateService(chatHistoryRepository, config.ReplicateClient, "spuuntries/urna-kp3l:9338a4573a17178b70515c0ef2e613d3b4213e2dc860ef23b3ad6149dacadc1e")
-	predictionService := services.NewPredictionService(chatHistoryRepository, replicateService, openAIService)
+	openAIService := services.NewOpenAIService(
+		chatHistoryRepository,
+		config.OpenAIClient,
+	)
+	replicateService := services.NewReplicateService(
+		chatHistoryRepository,
+		config.ReplicateClient,
+		"spuuntries/urna-kp3l:9338a4573a17178b70515c0ef2e613d3b4213e2dc860ef23b3ad6149dacadc1e",
+	)
+	predictionService := services.NewPredictionService(
+		chatHistoryRepository,
+		replicateService,
+		openAIService,
+	)
 	predictionController := controller.NewPredictionController(predictionService)
-
 	return predictionController
 }
